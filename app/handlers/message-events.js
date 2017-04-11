4e6232d0629d013c169bb507eee93187f7eabe3d
Clayton Fidelis <clayton.fidelis@codeminer42.com>
2017-04-11 13:55:49 -0300

    Add session and uuid

--- app/handlers/message-events.js
+++ app/handlers/message-events.js
@@ -1,4 +1,5 @@
 import { textRequest } from '../api'
+import log from 'better-log'
 
 import messagingEventHandler from '../messages/message-events'
 import quickReplyEventHandler from '../messages/quick-reply-events'
@@ -6,14 +7,13 @@ import postbackEventHandler from '../messages/postback-events'
 
 export default (req, res) => {
   let messaging_events = req.body.entry[0].messaging
-
   messaging_events.forEach(event => {
     let sender = event.sender.id
 
     if(event.message && event.message.text && !!!event.message.quick_reply) {
       let { text } = event.message
 
-      textRequest(text)
+      textRequest(text, req.sessionID)
         .then(res => {
           messagingEventHandler(res, event, sender)
         })
